cccb8d97914642d8fb0e259c187d85a713fab5ca
ITLUCDEV <it.lucdeveloper@gmail.com>
2023-05-30 12:46:32 +0200

    09 - Gestión de errores

--- 09- Gestion de errores/index.js
+++ 09- Gestion de errores/index.js
@@ -4,3 +4,22 @@ logger.info("Hola, esto es un mensaje informativo")
 logger.debug("Hola, esto es mensaje de debug")
 logger.warn("Hola, esto es un mensaje de advertencia")
 logger.error("Hola, esto es un error")
+
+function suma(a, b){
+    if (typeof a === "number" && typeof b === "number"){
+        return a + b
+    } else {
+        throw new Error("El valor no es un número")
+    }
+}
+
+const a = 4
+const b = "hola"
+
+try {
+    const sumar = suma(a, b)
+    console.log(sumar)
+} catch(e) {
+    logger.error(e)
+    logger.error("El valor introducido es incorrecto")
+}
